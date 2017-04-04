71798f7e76fd0ea49838e3596f59eb04fa1d50da
Antonio Cafiero <tcafiero@hotmail.com>
2017-04-04 14:55:52 +0200

    Release

--- Invoke.ps1
+++ Invoke.ps1
@@ -1,26 +1,50 @@
 param(
-  [string]$Username = 'Administrator',
-  [string]$Password = '7fMTKtvR',
-  [string]$ComputerName = "10.246.20.41",
-  [string]$Repository = "10.246.20.39",
-  [string]$HUBADDRESS = "10.246.40.179",
+  [string]$VMUsername = 'Administrator',
+  [string]$VMPassword = '7fMTKtvR',
+  [string]$VMName = "10.246.20.41",
+  [string]$SharedFolderUser = 'Administrator',
+  [string]$SharedFolderPassword = '7fMTKtvR',
+  [string]$ComputerRepository = "10.246.20.39",
+  [string]$DirSoftwareToInstall = "SoftwareToInstall",
+  [string]$DirDrivers = "drivers",
+  [string]$DirPrograms = "programs",
+  [string]$DirDestination = "C:\ToolChainPrograms",
+  [string]$Proxy = "proxy.volvocars.net",
+  [string]$ProxyPort = "83",
+  [string]$ProxyUser = "acafiero",
+  [string]$ProxyPassword = "Simone01",
+  [string]$HUBADDRESS = "10.244.181.145",
   [string]$HUBPORT = "7575",
   [string]$DEVICEADDRESS = "GOT100BQCZPF2.50"
 )
 
+
 $p = @{
-ComputerRepository=$Repository
-PHUBADDRESS=$HUBADDRESS
-PHUBPORT=$HUBPORT
-PDEVICEADDRESS=$DEVICEADDRESS
+VMUsername = $VMUsername
+VMPassword = $VMPassword
+VMName = $VMName
+SharedFolderUser = $SharedFolderUser
+SharedFolderPassword = $SharedFolderPassword
+ComputerRepository = $ComputerRepository
+DirSoftwareToInstall = $DirSoftwareToInstall
+DirDrivers = $DirDrivers
+DirPrograms = $DirPrograms
+DirDestination = $DirDestination
+Proxy = $Proxy
+ProxyPort = $ProxyPort
+ProxyUser = $ProxyUser
+ProxyPassword = $ProxyPassword 
+HUBADDRESS = $HUBADDRESS
+HUBPORT = $HUBPORT
+DEVICEADDRESS = $DEVICEADDRESS
 }
 
 $ScriptPath = '.\script.ps1'
 $sb = [scriptblock]::create(".{$(get-content $ScriptPath -Raw)} $(&{$args} @p)")
 
 
-$pass = ConvertTo-SecureString -AsPlainText $Password -Force
-$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
-$s = New-PSSession -ComputerName ($ComputerName) -Credential $Cred
+$pass = ConvertTo-SecureString -AsPlainText $VMPassword -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $VMUsername,$pass
+$s = New-PSSession -ComputerName ($VMName) -Credential $Cred
 #Invoke-Command -Session $s -FilePath .\script.ps1
 Invoke-Command -Session $s -ScriptBlock $sb
