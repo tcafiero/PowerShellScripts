3a1106045bfa02db16ee78849dba88248dc756cd
Antonio Cafiero <tcafiero@hotmail.com>
2017-04-24 10:48:04 +0200

    Release

--- InvokeRemotely.ps1
+++ InvokeRemotely.ps1
@@ -2,19 +2,19 @@ param(
   [string]$VMUsername = 'Administrator',
   [string]$VMPassword = '7fMTKtvR',
   [string]$VMName = "10.246.20.41",
-  [string]$Script = '.\script.ps1'
+  [string]$Script = '.\InstallDongleMgmtClient.ps1'
 )
 
 
 $p = @{
 }
 
-#$ScriptPath = '.\script.ps1'
+#$ScriptPath = '.\InstallDongleMgmtClient.ps1'
 $sb = [scriptblock]::create(".{$(get-content $Script -Raw)} $(&{$args} @p)")
 
 
 $pass = ConvertTo-SecureString -AsPlainText $VMPassword -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $VMUsername,$pass
 $s = New-PSSession -ComputerName ($VMName) -Credential $Cred
-#Invoke-Command -Session $s -FilePath .\script.ps1
+#Invoke-Command -Session $s -FilePath $Script
 Invoke-Command -Session $s -ScriptBlock $sb
